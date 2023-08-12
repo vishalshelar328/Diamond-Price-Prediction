fa68b83db45c5fd6c251d4e7577f418a0bf895b1
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-08-12 17:01:33 +0530

    application2.py

--- application2.py
+++ application2.py
@@ -1,23 +1,51 @@
+import streamlit as st
+from src.pipelines.prediction_pipeline import CustomData, PredictPipeline
+from src.exception import CustomException
+
+import pandas as pd
+
+class CustomData:
+    def __init__(self,
+                 carat=float(0.5),
+                 depth=float(60),
+                 table=float(55),
+                 x=float(5),
+                 y=float(5),
+                 z=float(5),
+                 cut='Fair',
+                 color='D',
+                 clarity='I1'):
+        self.carat = carat
+        self.depth = depth
+        self.table = table
+        self.x = x
+        self.y = y
+        self.z = z
+        self.cut = cut
+        self.color = color
+        self.clarity = clarity
+
+    def get_data(self):
+        return {
+            'carat': self.carat,
+            'depth': self.depth,
+            'table': self.table,
+            'x': self.x,
+            'y': self.y,
+            'z': self.z,
+            'cut': self.cut,
+            'color': self.color,
+            'clarity': self.clarity
+        }
+
 class CustomException(Exception):
     def __init__(self, error_message, error_detail):
         super().__init__(error_message)
         self.error_detail = error_detail
 
 def main():
-    st.title('Diamond Prediction App')
 
-    st.write('Fill in the following details:')
-    
-    carat = st.text_input('Carat:', value='0.5')
-    depth = st.text_input('Depth:', value='60')
-    table = st.text_input('Table:', value='55')
-    x = st.text_input('x:', value='5')
-    y = st.text_input('y:', value='5')
-    z = st.text_input('z:', value='5')
-    
-    cut = st.selectbox('Cut:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
-    color = st.selectbox('Color:', ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
-    clarity = st.selectbox('Clarity:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
+    st.title('Diamond Prediction App')
 
     if st.button('Predict'):
         try:
