99e6a39562388416ed851d78c9f89cba130587dd
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-08-12 13:14:12 +0530

    application2.py

--- application2.py
+++ application2.py
@@ -18,24 +18,28 @@ def main():
     clarity = st.selectbox('Clarity:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
 
     if st.button('Predict'):
-        data = CustomData(
-            carat=float(carat),
-            depth=float(depth),
-            table=float(table),
-            x=float(x),
-            y=float(y),
-            z=float(z),
-            cut=cut,
-            color=color,
-            clarity=clarity
-        )
-        final_new_data = data.get_data_as_dataframe()
-        predict_pipeline = PredictPipeline()
-        pred = predict_pipeline.predict(final_new_data)
-        results = round(pred[0], 2)
+        try:
+            data = CustomData(
+                carat=float(carat),
+                depth=float(depth),
+                table=float(table),
+                x=float(x),
+                y=float(y),
+                z=float(z),
+                cut=cut,
+                color=color,
+                clarity=clarity
+            )
+            final_new_data = data.get_data_as_dataframe()
+            predict_pipeline = PredictPipeline()
+            pred = predict_pipeline.predict(final_new_data)
+            results = round(pred[0], 2)
 
-        st.write('### Prediction Result:')
-        st.write(f'Predicted Diamond Price: {results}')
+            st.write('### Prediction Result:')
+            st.write(f'Predicted Diamond Price: {results}')
+        except Exception as e:
+            st.error("An error occurred while processing the prediction.")
+            st.error(f"Error details: {e}")
 
 if __name__ == "__main__":
     main()
