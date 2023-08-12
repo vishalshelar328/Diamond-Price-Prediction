bc40939c3310ffbad4d8dc569967fe40799beed0
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-08-12 15:53:16 +0530

    exception.py

--- src/exception.py
+++ src/exception.py
@@ -11,14 +11,15 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+
 class CustomException(Exception):
-    
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message, error_detail):
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+        self.error_message = f"{error_message} - Error Detail: {error_detail}"
 
     def __str__(self):
-        return self.error_message  
+        return self.error_message
+
     
 
 if __name__=="__main__":
