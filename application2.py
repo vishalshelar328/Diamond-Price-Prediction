4996ee8c66307101d34b1d6651438725c03e7246
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-08-12 13:20:45 +0530

    application2.py

--- application2.py
+++ application2.py
@@ -6,40 +6,36 @@ def main():
 
     st.write('Fill in the following details:')
     
-    carat = st.text_input('Carat:', value='')
-    depth = st.text_input('Depth:', value='')
-    table = st.text_input('Table:', value='')
-    x = st.text_input('x:', value='')
-    y = st.text_input('y:', value='')
-    z = st.text_input('z:', value='')
+    carat = st.text_input('Carat:', value='0.5')
+    depth = st.text_input('Depth:', value='60')
+    table = st.text_input('Table:', value='55')
+    x = st.text_input('x:', value='5')
+    y = st.text_input('y:', value='5')
+    z = st.text_input('z:', value='5')
     
     cut = st.selectbox('Cut:', ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'])
     color = st.selectbox('Color:', ['D', 'E', 'F', 'G', 'H', 'I', 'J'])
     clarity = st.selectbox('Clarity:', ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'])
 
     if st.button('Predict'):
-        try:
-            data = CustomData(
-                carat=float(carat),
-                depth=float(depth),
-                table=float(table),
-                x=float(x),
-                y=float(y),
-                z=float(z),
-                cut=cut,
-                color=color,
-                clarity=clarity
-            )
-            final_new_data = data.get_data_as_dataframe()
-            predict_pipeline = PredictPipeline()
-            pred = predict_pipeline.predict(final_new_data)
-            results = round(pred[0], 2)
+        data = CustomData(
+            carat=float(carat),
+            depth=float(depth),
+            table=float(table),
+            x=float(x),
+            y=float(y),
+            z=float(z),
+            cut=cut,
+            color=color,
+            clarity=clarity
+        )
+        final_new_data = data.get_data_as_dataframe()
+        predict_pipeline = PredictPipeline()
+        pred = predict_pipeline.predict(final_new_data)
+        results = round(pred[0], 2)
 
-            st.write('### Prediction Result:')
-            st.write(f'Predicted Diamond Price: {results}')
-        except Exception as e:
-            st.error("An error occurred while processing the prediction.")
-            st.error(f"Error details: {e}")
+        st.write('### Prediction Result:')
+        st.write(f'Predicted Diamond Price: {results}')
 
 if __name__ == "__main__":
     main()
