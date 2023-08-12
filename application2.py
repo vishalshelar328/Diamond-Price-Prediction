acdeb93538fcfda55fc8758bcb8259a62bf97a6f
Vishal Shelar <124070302+vishalshelar328@users.noreply.github.com>
2023-08-12 17:08:55 +0530

    application2.py

--- application2.py
+++ application2.py
@@ -32,7 +32,7 @@ def main():
                 color=color,
                 clarity=clarity
             )
-            final_new_data = pd.DataFrame(data.get_data())
+            final_new_data = data.get_data_as_dataframe()
             predict_pipeline = PredictPipeline()
             pred = predict_pipeline.predict(final_new_data)
             results = round(pred[0], 2)
